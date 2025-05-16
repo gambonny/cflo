56b1949a205e6deacaba757af9bb71d65a69f000
Jonny Gamba <gambonny@gmail.com>
2025-05-15 20:49:44 -0500

    fix: put back return type for getSupportedLevels function

--- src/levels.ts
+++ src/levels.ts
@@ -3,7 +3,7 @@ import type { LoggerConfig } from "@/types"
 const logLevels = ["debug", "info", "log", "warn", "error"] as const
 const levelsOrdered = { debug: 0, info: 1, log: 2, warn: 3, error: 4 }
 
-export function getSupportedLevels() {
+export function getSupportedLevels(): readonly (typeof logLevels)[number][] {
 	return logLevels
 }
 
